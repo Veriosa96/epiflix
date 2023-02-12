45991e97910befbaad3dcd204336b2e8f0ae56ff
Saverio Caracappa <saveriocaracappa@gmail.com>
2023-02-12 16:20:42 +0100

    add footer

--- src/App.js
+++ src/App.js
@@ -2,13 +2,16 @@ import './App.css';
 import NavBar from './componenti/NavBar';
 import SecondNavBar from './componenti/SecondNavBar';
 import FilmList from './componenti/FilmList';
+import Footer from './componenti/Footer';
+
 
 function App() {
   return (
-    <div className="App" style={{backgroundColor: "#221f1f"}}>
+    <div className="App" style={{backgroundColor: "#221f1f"}}>     
       <NavBar/>
       <SecondNavBar/>
       <FilmList/>
+      <Footer/>     
     </div>
   );
 }
--- src/componenti/NavBar.jsx
+++ src/componenti/NavBar.jsx
@@ -1,8 +1,7 @@
 import "bootstrap/dist/css/bootstrap.min.css";
 import React from "react";
-import Container from "react-bootstrap/Container";
-import Nav from "react-bootstrap/Nav";
-import Navbar from "react-bootstrap/Navbar";
+import {Container,Nav,Navbar} from "react-bootstrap";
+
 // import NavDropdown from "react-bootstrap/NavDropdown";
 
 const NavBar = () => {
