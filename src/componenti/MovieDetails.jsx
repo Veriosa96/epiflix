24d65f56e4fcfa792fa7a799b1827a7d8f9b594f
Saverio Caracappa <saveriocaracappa@gmail.com>
2023-02-15 16:58:32 +0100

    .

---
+++ src/componenti/MovieDetails.jsx
@@ -0,0 +1,35 @@
+import { useEffect, useState } from "react";
+import { useParams } from "react-router-dom";
+import{Container, Row,Col} from "react-bootstrap";
+
+
+const MovieDetails = () => {}
+const params = useParams()
+console.log("PARAMS",params)
+const [movie, setMovie] = useState(null)
+
+
+useEffect(() => {
+    const movieObj=SingleMovie.find(movie=>movie.id.toString()===params.movieId)
+    console.log("MOVIE",movieObj)
+},[]
+return(
+    <Container>
+        <Row>
+
+            
+            <Col>
+            movie? (
+                <div>
+                    <h2>{movie.name}</h2>
+                </div>
+            )
+
+            </Col>
+        </Row>
+    </Container>
+)
+);
+
+
+export default MovieDetails;
